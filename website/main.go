d055fb1668e09156c16df02c7fcac5e7f86611b3
Cesar Augusto <caherdenez@gmail.com>
2017-11-26 11:46:34 -0500

    add learn gorutines

---
+++ website/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "github.com/cesar0510/golang_api/website/models"
+
+
+)
+
+var lista []int
+var data map[string]string
+
+func main() {
+
+    tuple := [4]int{2,4,5,6}
+    data :=make(map[string]string)
+    data["nombre"] = "Cesar"
+    cesar = models.User{"Cesar","Herdenez"}
+
+
+    fmt.Println(tuple)
+    fmt.Println(cesar)
+    fmt.Println(data)
+
+}
